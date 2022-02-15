ac24013e8879b011ccba8aa124c3e90d59f99e7b
mariobasurto <mariobasurto9@gmail.com>
2022-02-15 17:40:12 -0600

    Terminado el ultimo video

--- lambda.rb
+++ lambda.rb
@@ -1,8 +1,14 @@
 def test_lambda
+    (->() {return "Game over"}).call()
     puts "Despues de la lambda"
 end
 
 def test_block
-
+    (Proc.new {return "Game over"}).call()
     puts "Despues del bloque"
 end
+
+puts test_lambda
+puts test_block
+
+gets()
